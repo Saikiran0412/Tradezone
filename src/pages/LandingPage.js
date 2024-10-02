47006a7d75dbcc62e98c4f2b1dae0b7e1101d93b
Saikiran0412 <chsaikiran0412@gmail.com>
2024-10-02 13:12:57 -0500

    Add initial folder structure

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import "../pages/landingPage.css"
+export default function LandingPage(){
+
+
+    let number = 2;
+    return (
+        <div className="landing-page-container">
+            <h1>This is landing page {number} </h1>
+        </div>
+    )
+}
